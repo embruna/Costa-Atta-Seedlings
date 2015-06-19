358aea91bfa6354d1d4e3ecf7569f6c119bdf6a4
embruna <embruna@ufl.edu>
2015-06-19 16:10:11 -0400

    code for figures

--- Costa_etal_Analyses.R
+++ Costa_etal_Analyses.R
@@ -4,7 +4,6 @@
 
 # library(gdata)
 # library(lme4)
-# library(ggplot2)
 # library(reshape)
 # library(maps)
 # library(WDI)
@@ -14,9 +13,10 @@
 # library(reshape)
 # library(gridExtra)
 # library(emdbook)
-
+library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(grid)
 #CLear out everything from the environment
 rm(list=ls())
 
@@ -40,6 +40,83 @@ summary(AIRTEMPHUMID)
 str(NEST.DATA)
 str(VEG)
 str(AIRTEMPHUMID)
+
+
+
+##########################
+####### FIGURES ##########
+##########################
+
+
+## Air Temp and Humidity near and far from nests in CR and CD over the course of 24 hours
+
+# count.ATH<-as.data.frame(table(AIRTEMPHUMID$nest, AIRTEMPHUMID$habitat)) #This is just to see how many nests in each habitat type
+ATH.FIG<-na.omit(AIRTEMPHUMID)  #create a dataframe to make this figure
+
+# #Time needs to be a factor so you can plot it as the x axis
+# ATH.FIG$time<-as.factor(ATH.FIG$time)
+
+#change code to complete names so figure legends look nicer
+levels(ATH.FIG$habitat)[levels(ATH.FIG$habitat)=="CR"]<-"Cerrado Ralo"  
+levels(ATH.FIG$habitat)[levels(ATH.FIG$habitat)=="CD"]<-"Cerrado Denso"  
+levels(ATH.FIG$location)[levels(ATH.FIG$location)=="away"]<-"10m from nest"  
+levels(ATH.FIG$location)[levels(ATH.FIG$location)=="nest"]<-"Center of nest"  
+
+# Calculate the average of air temp for nests in each habitat in each location at each time point
+ATH.FIG<-ATH.FIG %>% group_by(habitat, location, time) %>% summarise(mean(temp), sd(temp), mean(air.humid), sd(air.humid))
+ATH.FIG<-as.data.frame(ATH.FIG)
+#rename the columns
+colnames(ATH.FIG)[4]<-c("Temperature")
+colnames(ATH.FIG)[5]<-c("SD.temperature")
+colnames(ATH.FIG)[6]<-c("Humidity")
+colnames(ATH.FIG)[7]<-c("SD.humidity")
+colnames(ATH.FIG)[1]<-c("Habitat")
+colnames(ATH.FIG)[2]<-c("Location")
+
+
+# Use ggplot to make the figures.  this one is a more basic qplot. The following line greates a line plot of publication 
+#this will shoft the error bars a bit so they don't overlap with "position dodge"
+pd<-position_dodge(0.4)
+
+AirTempFig<- ggplot(ATH.FIG, aes(x = time, y = Temperature, color = location)) +
+  geom_errorbar(aes(ymin=Temperature-SD.temperature, ymax=Temperature+SD.temperature), width=.75, position=pd)+
+  ylab("Air temperature (nest plots, mean SD)") +xlab("Hour")+geom_line()+geom_point(size=3) +
+  facet_wrap( ~ Habitat, nrow=1, scales="free_x")+ggtitle("A")      #scales="free_x" keeps the same y axis for all facets but allows you to modify x axis
+
+AirTempFig<-AirTempFig + scale_colour_manual(values=c("red4", "darkblue"))  #I chose my own colors for the lines
+AirTempFig<-AirTempFig + geom_line(aes(group=factor(location)),size=.75)  #Changes the thickness of the lines
+AirTempFig<-AirTempFig + scale_x_continuous(breaks = seq(0, 23, 2), limits = c(0, 23))
+AirTempFig<-AirTempFig + theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(), 
+                                       panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"), #sets colors of axes
+                                       plot.title = element_text(hjust=0.01, vjust=2, face="bold", size=20),        #Sets title size, style, location
+                                       axis.title.x=element_text(colour="black", size = 18, vjust=-2),            #sets x axis title size, style, distance from axis #add , face = "bold" if you want bold
+                                       axis.title.y=element_text(colour="black", size = 18, vjust=2),            #sets y axis title size, style, distance from axis #add , face = "bold" if you want bold
+                                       axis.text=element_text(colour="black", size = 16),
+                             strip.text.x = element_text(size=16, colour="black", vjust=-1, face="bold"),
+                             plot.margin = unit(c(1,3,3,1), "cm"),   
+                             panel.margin = unit(1.5, "lines"),    #Adds more space between the facets (=panels)
+                             strip.background = element_blank(),
+                             legend.title=element_blank(),
+                             legend.text = element_text(color="black", size=16),
+                             legend.key = element_blank(),  #removes box around legend subunits
+                             legend.position = c(0.97,0.9),   #plot margin
+                             panel.border = element_rect(fill=NA, colour = "black", size=1, linetype="solid"))+
+  guides(colour=guide_legend(override.aes=list(size=3)))+                        #Removes the boxes around legend colors       
+  guides(fill = guide_legend(reverse=TRUE))
+AirTempFig
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Use Cerrado Ralo (CR) and Cerrado Denso (CD) in analyse
 NEST.DATA_both<-NEST.DATA[NEST.DATA$habitat=="CR"|NEST.DATA$habitat=="CD",] #both habitats
@@ -113,5 +190,32 @@ qqnorm(resid(fm1))
 mp1 <- glmer((grass.bmass+1) ~ location + (1 | nest), data=NEST.DATA_CD, family="Gamma")
 
 
+##########################################
+## SANDBOX
+##########################################
 
+
+#Line plot
+#note the filter means only doing with nest plots
+MyATHfig<-ggplot(filter(ATH.FIG, Habitat=="Cerrado Ralo"), aes(x = time, y = Temperature, col=location)) + 
+  geom_errorbar(aes(ymin=Temperature-SD.temperature, ymax=Temperature+SD.temperature), width=.75, position=pd)+
+  ylab("Air temperature (nest plots, mean SD)") +xlab("Hour")+geom_line()+geom_point(size=3) 
+
+MyATHfig<-MyATHfig+ggtitle("A) Cerrado ralo")
+MyATHfig<-MyATHfig + scale_colour_manual(values=c("darkred", "darkblue"))  #I chose my own colors for the lines
+MyATHfig<-MyATHfig + geom_line(aes(group=factor(location)),size=.75)  #Changes the thickness of the lines
+MyATHfig<-MyATHfig + scale_x_continuous(breaks = seq(0, 23, 1), limits = c(0, 23))
+MyATHfig<- MyATHfig + theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(), 
+                                       panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"), #sets colors of axes
+                                       plot.title = element_text(hjust=0.05, vjust=-1.8, face="bold", size=20),        #Sets title size, style, location
+                                       axis.title.x=element_text(colour="black", size = 18, vjust=-2),            #sets x axis title size, style, distance from axis #add , face = "bold" if you want bold
+                                       axis.title.y=element_text(colour="black", size = 18, vjust=2),            #sets y axis title size, style, distance from axis #add , face = "bold" if you want bold
+                                       axis.text=element_text(colour="black", size = 16),                              #sets size and style of labels on axes
+                                       legend.title = element_blank(),                                  #Removes the Legend title
+                                       legend.key = element_blank(),                                  #Removes the boxes around legend colors
+                                       legend.text = element_text(color="black", size=16),
+                                       plot.margin =unit(c(2,2,2,2), "cm"),
+                                       legend.position = c(0.9,0.5))+   #plot margin
+  guides(colour=guide_legend(override.aes=list(size=3)))                        #size of legen bars                                                      
+MyATHfig
 
