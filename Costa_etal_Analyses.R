6e9382f05303bfaa0026e4a2b6fd470de4427174
embruna <embruna@ufl.edu>
2015-06-22 10:05:01 -0400

    hacking around to get the number of seedlings and species per plot, bind to % cover, and analyze

--- Costa_etal_Analyses.R
+++ Costa_etal_Analyses.R
@@ -76,8 +76,8 @@ VEG_both$nest<-as.factor(VEG_both$nest)
 summary(VEG_both)
 str(VEG_both)
 #Remove the NA
-VEG_both_summary<-na.omit(VEG_both)
-VEG_both_summary<-as.data.frame(VEG_both_summary)
+# VEG_both_summary<-na.omit(VEG_both)
+# VEG_both_summary<-as.data.frame(VEG_both_summary)
 summary(VEG_both_summary)
 str(VEG_both_summary)
 
@@ -86,38 +86,108 @@ str(VEG_both_summary)
 #   group_by(Species) %>%
 #   summarize(meanSepLength=mean(Sepal.Length)) 
 
-#VEG_both_summary$species<-toString(VEG_both_summary$species)
+# Hacking my way around to count how many seedlings in each plot
+# CORRECT WAY: 
+
+# A<-na.omit(VEG_both)
+# B<-select(A, nest, location, species)
+# count.df<-A %>%
+#   group_by(nest, location) %>%
+#   summarise(count = n())
+# count.df
+# but this leaves out the ones thathad 0, so you cant bind it easily to the vector of % cover.  
+# What you need is plyr's .drop=FALSE, but it doesn't exist in dplyr, so the following yould be how you work around.
+# dplyr solution to having .drop=FALSE to count plots with NA as having 0 plants in them
+# http://stackoverflow.com/questions/22523131/dplyr-summarise-equivalent-of-drop-false-to-keep-groups-with-zero-length-in
+# As an alternative, I will include plot.id, group, and count. This gives you the number of plots in your dataframe (with each plant having 
+# its associated plot in the long form table.  Yoiu then convert all 1 to 0 -> plots with no plants were counted as a single row
+# with an NA in it.  Ugly, but works
 
 
-#Number of plants in each plot  group by nest and location of plot and count 
-count.df<-VEG_both_summary %>%
+VEG_both_hack<-select(VEG_both, nest, location, species, plot.id)
+count.df<-VEG_both_hack %>%
   group_by(nest, location) %>%
-  summarise(count = n())
+  summarise(sdlg.no = n())
+count.df
+count.df$sdlg.no[count.df$sdlg.no==1]<-0
 count.df
 
 #too see hpw many species each plot had, first reshape so that the data are in wide fomr
 require(reshape2)
-spp.df.lomg<-select(VEG_both_summary, nest, location, species) #is there a dplyr equivalent?????
-spp.df.wide<-dcast(spp.df.lomg, nest+location ~ species)
+spp.df.long<-select(VEG_both, nest, location, species, plot.id) #is there a dplyr equivalent?????
+spp.df.wide<-dcast(spp.df.long, nest+location ~ species)
 spp.df.wide<-spp.df.wide[,c(3:197)]
 spp.df<-rowSums(spp.df.wide != 0)
+#checking to make sure number of rows is the same to cbind
 
-sdlgs<-cbind(count.df, spp.df)
+dim(count.df)
+dim(spp.df.wide)
+dim(NEST.DATA_both) 
+#add the column "plot.id" from NEST.DATA_both to create new summary df seedlings.  NB: THERE IS HIGH POTENTIAL FOR FUNCKING THINGS 
+# UP HERE, SO BE CAREFUL
+sdlgs<-cbind(NEST.DATA_both$plot.id, count.df)
+names(sdlgs)[1]<-"plot.id" #rename the column
+
+# THIS CREATES A DATAFRAME YOU CAN BIND TO THE % COVER
+sdlgs<-cbind(sdlgs, spp.df)
+names(sdlgs)[5]<-"spp.no" #rename the column
 str(sdlgs)
 
+# BIND THEM UP....add % cover!!!
+sdlgs.perc.cover<-select(NEST.DATA_both, plot.id, perc.cover)
+dim(sdlgs.perc.cover) #make sure same size as sdlgs dataframe
+sdlgs<-left_join(sdlgs, sdlgs.perc.cover, by = "plot.id")
 
-#add % cover
-sdlgs.perc.cover<-select(NEST.DATA, nest, location, perc.cover)
-str(sdlgs.perc.cover)
-test<-left_join(sdlgs, sdlgs.perc.cover, by = "location")
+#THIS WILL TELL YOU HOW MANY WE HAVE WITH AND WITHOUT % COVER
+# dplyr::count(sdlgs, location) 
+# vs. 
+# dplyr::count(na.omit(sdlgs), location)
+
+# SDLG COUNTS GLMS
+sdlg.glm.data<-na.omit(sdlgs)
+glm.sdlg1 = glm(sdlg.no ~ perc.cover*location+nest ,data=sdlg.glm.data,family=poisson) #Recall * is syntax syntax shortcue of both main effects + interaction
+summary(glm.sdlg1)
+
+glm.sdlg2 = glm(sdlg.no ~ location+nest ,data=sdlg.glm.data,family=poisson) #Recall * is syntax syntax shortcue of both main effects + interaction
+summary(glm.sdlg2)
+
+AIC(glm.sdlg1,glm.sdlg2)
+anova(glm.sdlg1,glm.sdlg2,test="Chisq")
+
+# SPP COUNTS GLMS
+sdlg.glm.data<-na.omit(sdlgs)
+glm.sdlg1 = glm(spp.no ~ perc.cover*location+nest ,data=sdlg.glm.data,family=poisson) #Recall * is syntax syntax shortcue of both main effects + interaction
+summary(glm.sdlg1)
+
+glm.sdlg2 = glm(spp.no ~ location+nest ,data=sdlg.glm.data,family=poisson) #Recall * is syntax syntax shortcue of both main effects + interaction
+summary(glm.sdlg2)
+
+AIC(glm.sdlg1,glm.sdlg2)
+anova(glm.sdlg1,glm.sdlg2,test="Chisq")
+
+###############
+### VISUALIZATIONS
+###############
+plot.sdlg.no<-ggplot(sdlg.glm.data, aes(x = perc.cover, y = sdlg.no, col=location, fill=location)) + 
+  geom_point(shape=16, size = 3) +
+  ylab("no of seedlings") +
+  xlab("canopy cover (%)")+
+  ggtitle("A")+
+  #scale_colour_hue(l=50) + # Use a slightly darker palette than normal
+  geom_smooth(method=lm,   # Add linear regression lines
+              se=FALSE) 
+plot.sdlg.no
 
 
-
-
-glm.slg = glm(count ~ location * cover,data=GLM.DATA,family=gaussian) #Recall * is syntax syntax shortcue of both main effects + interaction
-summary(glm4b)
-anova(glm1b,glm4b,test="Chisq")
-
+plot.spp.no<-ggplot(sdlg.glm.data, aes(x = perc.cover, y = spp.no, col=location, fill=location)) + 
+  geom_point(shape=16, size = 3) +
+  ylab("no of species") +
+  xlab("canopy cover (%)")+
+  ggtitle("B")+
+  #scale_colour_hue(l=50) + # Use a slightly darker palette than normal
+  geom_smooth(method=lm,   # Add linear regression lines
+              se=FALSE) 
+plot.spp.no
 
 ###############
 ### PAIRED TEST
